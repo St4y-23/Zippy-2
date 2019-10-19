4afa7c897ec03f9e4753e0d35f9894af829e232e
St4y-23 <ghost19011@gmail.com>
2019-10-19 09:47:43 +0400

    added all

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,33 @@ $(function(){
     slidesToScroll: 1,
   });
 
+  $(".video-slider__inner").slick({
+    centerMode: true,
+    centerPadding: '320px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+
+  $('.slider-life__top-inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider-life/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider-life/arrow-right.svg" alt=""></button>',
+    asNavFor: '.slider-life__bottom-inner',
+  });
+
+  $('.slider-life__bottom-inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-life__top-inner',
+    focusOnSelect: true,
+    arrows: false,
+    centerMode: true,
+  });
+
+  
+
+
 });
